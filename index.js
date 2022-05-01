6cfc6af01ca0c034b8b0a7dd7524a016db0c8dd4
dineshsellappan <dinisellappan@gmail.com>
2022-05-01 14:40:07 +0530

    index config changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('/',sendQuoteMail)
 if(process.env.NODE_ENV=='production'){
     app.use(express.static('techietom/build'));
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'techietom','build','inmdex.js'));
+        res.sendFile(path.resolve(__dirname,'techietom','build','index.js'));
     })
 }
 app.listen(3001,console.log('connected to 3001'))
