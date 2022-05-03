20caca055aa0d9fbfda1ef11ab4ad3e416ef689d
dinesh17e307 <64677391+dinesh17e307@users.noreply.github.com>
2022-05-03 11:49:04 +0530

    Delete server directory

--- server/Routes/SendQuotemail.js
+++
@@ -1,5 +0,0 @@
-const express=require('express')
-const sendQuoteMail=require('../controller/SendQuoteMail')
-const router=express.Router();
-router.post('/sendQuoteMail',sendQuoteMail.sendQuoteMail)
-module.exports=router
